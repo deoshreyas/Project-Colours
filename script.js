20d6a7bd0116348261ac1b43f7781ef653736afd
Shreyas Deo <shreyasdeo.k50@gmail.com>
2024-07-19 18:35:48 +0530

    Finish a responsive hero section using HTML Canvas, Work on a menu to change the default values of the colours used on website

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ function closeMenu() {
 function openMenu() {
     document.getElementById('menu').style.display = 'flex';
 }
+
+function drawHeroRight() {
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+    ctx.fillStyle = window.getComputedStyle(document.documentElement).getPropertyValue('--primary-button-col');
+    ctx.fillRect(50, 50, 100, 100);
+    ctx.fillStyle = window.getComputedStyle(document.documentElement).getPropertyValue('--accent-col');
+    ctx.fillRect(50, 30, 125, 15);
+    ctx.fillStyle = window.getComputedStyle(document.documentElement).getPropertyValue('--secondary-button-col');
+    ctx.fillRect(155, 50, 20, 100);
+}
+
+drawHeroRight();
+
+document.querySelectorAll('.col-input input[type="color"]').forEach(function (input) {
+    input.addEventListener('change', function () {
+        drawHeroRight();
+    });
+});
