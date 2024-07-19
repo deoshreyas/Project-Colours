707febbed141167744d3a7d1727b18a33642f515
Shreyas Deo <shreyasdeo.k50@gmail.com>
2024-07-20 00:34:36 +0530

    Fix bugs in responsive UI on phone, Add colour picker responsiveness and fix more bugs

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 function closeMenu() {
     document.getElementById('menu').style.display = 'none';
+    document.querySelector('body').style.overflow = 'scroll';
 }
 
 function openMenu() {
     document.getElementById('menu').style.display = 'flex';
+    document.querySelector('body').style.overflow = 'hidden';
 }
 
 function drawHeroRight() {
     var canvas = document.getElementById('canvas');
     var ctx = canvas.getContext('2d');
     ctx.fillStyle = window.getComputedStyle(document.documentElement).getPropertyValue('--primary-button-col');
-    ctx.fillRect(50, 50, 100, 100);
+    ctx.fillRect(0, 25, canvas.width/2, canvas.height - (canvas.height/8 + 5));
     ctx.fillStyle = window.getComputedStyle(document.documentElement).getPropertyValue('--accent-col');
-    ctx.fillRect(50, 30, 125, 15);
+    ctx.fillRect(0, 0, canvas.width/2, canvas.height/8);
     ctx.fillStyle = window.getComputedStyle(document.documentElement).getPropertyValue('--secondary-button-col');
-    ctx.fillRect(155, 50, 20, 100);
+    ctx.fillRect(canvas.width/2 + 5, 0, canvas.width/2 - 5, canvas.height);
 }
 
 drawHeroRight();
@@ -24,9 +26,11 @@ function openColourOptions() {
     if (!options_open) {
         document.getElementsByClassName('colour-options')[0].style.display = 'flex';
         options_open = true;
+        document.querySelector('body').style.overflow = 'hidden';
     } else {
         document.getElementsByClassName('colour-options')[0].style.display = 'none';
         options_open = false;
+        document.querySelector('body').style.overflow = 'scroll';
     }
 }
 
